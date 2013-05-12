356193ae09a000eb3415443cc91930cb27bde128
Philip Leonard <phillyleonard@gmail.com>
2013-05-12 16:02:29 +0800

    Removing server files to then upload to rename properly (Git bug?)

--- LetMeInServer/src/theServer.java
+++
@@ -1,9 +0,0 @@
-
-public class TheServer extends Thread {
-	public static void main(String[] args) {
-		
-		Thread t = new Thread(new ListeningClass()); //listens on port 8080
-		t.start();
-		
-	}
-}
